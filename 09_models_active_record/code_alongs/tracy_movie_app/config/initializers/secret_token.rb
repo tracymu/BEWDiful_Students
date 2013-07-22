26138eb5443a728c31f7a551659009765468d2ae
tracymu <tracy.musung@moomumedia.com>
2013-07-22 19:04:05 +1000

    Movie app first commit

---
+++ 09_models_active_record/code_alongs/tracy_movie_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TracyMovieApp::Application.config.secret_key_base = 'c458d98ce8ce8a88df68f6f6a46ee00a6823bda308781137a250c24495d56a51ae3d71c32d0ddde9e7062c318e3d5a79280eb2e243ba855322748143b4c03cbc'
---
+++ 09_models_active_record/code_alongs/tracy_movie_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TracyMovieApp::Application.config.session_store :cookie_store, key: '_tracy_movie_app_session'
