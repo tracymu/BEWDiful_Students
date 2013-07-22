54292ceabba126b763de4f5fb418256e0b6a5a28
Ben Askins <ben.askins@gmail.com>
2013-07-22 22:33:04 +1000

    Shirts app

---
+++ 09_models_active_record/code_alongs/ben_shirts_app/app/models/shirt.rb
@@ -0,0 +1,3 @@
+class Shirt < ActiveRecord::Base
+  attr_accessible :name
+end
---
+++ 09_models_active_record/code_alongs/ben_shirts_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BenShirtsApp::Application.config.session_store :cookie_store, key: '_ben_shirts_app_session'
