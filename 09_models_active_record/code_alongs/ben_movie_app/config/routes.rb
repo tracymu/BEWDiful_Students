0b2cdfffa39b21cfedd8f715aecc833bbfbf2336
Ben Askins <ben.askins@gmail.com>
2013-08-01 08:17:44 +1000

    Devise example

--- 09_models_active_record/code_alongs/ben_movie_app/config/routes.rb
+++ 09_models_active_record/code_alongs/ben_movie_app/config/routes.rb
@@ -1,5 +1,9 @@
 BenMovieApp::Application.routes.draw do
+  devise_for :users
+  
   root 'welcome#index'
+  
+  resources :movies
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
