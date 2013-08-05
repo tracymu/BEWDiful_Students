b9461e5494722dec9f1ebaf014a647deb868674f
Ben Askins <ben.askins@gmail.com>
2013-08-06 09:33:28 +1000

    Add the comments table

--- 13_Associations/code_alongs/rewsly/db/schema.rb
+++ 13_Associations/code_alongs/rewsly/db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130805101553) do
+ActiveRecord::Schema.define(version: 20130805104208) do
+
+  create_table "comments", force: true do |t|
+    t.text     "content"
+    t.integer  "user_id"
+    t.integer  "story_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "stories", force: true do |t|
     t.string   "headline"
