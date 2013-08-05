bf12af6d5e7cfcac4a5363930210e10d38b6271e
Ben Askins <ben.askins@gmail.com>
2013-08-05 17:19:27 +1000

    Create new stories

--- 13_Associations/code_alongs/rewsly/app/controllers/stories_controller.rb
+++ 13_Associations/code_alongs/rewsly/app/controllers/stories_controller.rb
@@ -7,4 +7,19 @@ class StoriesController < ApplicationController
   def show
     @story = Story.find(params[:id])
   end
+
+
+  def new
+    @story = Story.new
+  end
+
+
+  def create
+    @story = Story.new params.require(:story).permit(:headline, :content)
+    if @story.save
+      redirect_to story_path(@story), :notice => "Story successfully created"
+    else
+      render 'new'
+    end
+  end
 end
